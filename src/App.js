d1651f3e4de86f1f50e67c92fcc0e9417a3abb69
Lisa <elizabethpham726@gmail.com>
2022-12-20 12:48:41 -0800

    adds API functions to App.js and gets API data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
 
-// const TASKS = [
-//   {
-//     id: 1,
-//     title: 'Mow the lawn',
-//     isComplete: false,
-//   },
-//   {
-//     id: 2,
-//     title: 'Cook Pasta',
-//     isComplete: true,
-//   },
-// ];
+const kBaseUrl = 'https://task-list-api-c17.herokuapp.com';
+
+const taskApiToJson = (task) => {
+  const { id, is_complete: isComplete, title } = task;
+
+  return { id, isComplete, title };
+};
+
+const getTasksApi = () => {
+  return axios
+    .get(`${kBaseUrl}/tasks`)
+    .then((response) => {
+      return response.data.map(taskApiToJson);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error('error fetching tasks');
+    });
+};
+
+const updateTaskApi = (id, markComplete) => {
+  const endpoint = markComplete ? 'mark_complete' : 'mark_incomplete';
+
+  return axios
+    .patch(`${kBaseUrl}/tasks/${id}/${endpoint}`)
+    .then((response) => {
+      return taskApiToJson(response.data.task);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error(`error updating task ${id}`);
+    });
+};
+
+const deleteTaskApi = (id) => {
+  return axios.delete(`${kBaseUrl}/tasks/${id}`).catch((error) => {
+    console.log(error);
+    throw new Error(`error deleting task ${id}`);
+  });
+};
 
 const App = () => {
   const [taskData, setTaskData] = useState([
@@ -30,15 +59,33 @@ const App = () => {
     },
   ]);
 
+  useEffect(() => {
+    refreshTasks();
+  }, []);
+
+  const refreshTasks = () => {
+    return getTasksApi()
+      .then((tasks) => {
+        setTaskData(tasks);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   const completeTask = (id) => {
-    const newTasks = taskData.map((task) => {
+
+    
+    return updateTaskApi(id, !task.isComplete)
+    .then(newTasks = taskData.map((task) => {
       if (task.id === id) {
         return { ...task, isComplete: !task.isComplete };
       } else {
         return task;
       }
-    });
-    setTaskData(newTasks);
+      });
+      setTaskData(newTasks);
+    );
   };
 
   const deleteTask = (id) => {
